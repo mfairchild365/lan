7d4b063a2df27742fed8a7338e5960cc039bc337
mfairchild365 <mfairchild365@gmail.com>
2012-12-13 19:50:28 -0600

    add a basic index page

--- www/index.php
+++ www/index.php
@@ -1,1 +1,18 @@
-<?php
+<html>
+    <head>
+        <script>
+            try {
+                var conn = new WebSocket('ws://192.168.1.131:8000/lan');
+                console.log('WebSocket - status '+conn.readyState);
+                conn.onopen = function(e) {
+                    console.log("Connection established!");
+                };
+                conn.onmessage = function(e) {
+                    console.log(e.data);
+                };
+            } catch(ex){
+                console.log(ex);
+            }
+        </script>
+    </head>
+</html>
