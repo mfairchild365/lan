08d20cb8c052aff22fed3cc0191b35fcddedae04
mfairchild365 <mfairchild365@gmail.com>
2012-12-14 19:43:00 -0600

    Add a function to connect to the DB

--- src/LAN/Util.php
+++ src/LAN/Util.php
@@ -19,4 +19,27 @@ class Util
 
         return false;
     }
+
+    /**
+     * Connect to the database and return it
+     *
+     * @throws \Exception
+     * @return mysqli
+     */
+    public static function getDB()
+    {
+        //Reduce connections by saving the current connection and reusing it.
+        static $db = false;
+
+        if (!$db) {
+            $db = new \mysqli(Config::get('DB_HOST'), Config::get('DB_USER'), Config::get('DB_PASSWORD'), Config::get('DB_NAME'));
+            if (mysqli_connect_error()) {
+                throw new \Exception('Database connection error (' . mysqli_connect_errno() . ') '
+                    . mysqli_connect_error());
+            }
+            $db->set_charset('utf8');
+        }
+
+        return $db;
+    }
 }
