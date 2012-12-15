f318e0d4e5ebeff2d48a594100d48b7e507a30a9
mfairchild365 <mfairchild365@gmail.com>
2012-12-14 19:42:09 -0600

    Use correct namespace

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,6 @@
 <?php
+use LAN\Config;
+
 /**********************************************************************************************************************
 * autoload and include path
 */
@@ -30,3 +32,11 @@ set_include_path(
 ini_set('display_errors', false);
 
 error_reporting(E_ALL);
+
+/**********************************************************************************************************************
+ * DB related settings
+ */
+Config::set('DB_HOST'     , 'localhost');
+Config::set('DB_USER'     , 'user');
+Config::set('DB_PASSWORD' , 'password');
+Config::set('DB_NAME'     , 'lan');
