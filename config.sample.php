8ca30afaa28af7c1e671066a3d17037accb1be68
mfairchild365 <mfairchild365@gmail.com>
2012-12-17 19:46:35 -0600

    Add Ratchet
    Use composer and use its auto-loader

--- config.sample.php
+++ config.sample.php
@@ -4,26 +4,7 @@ use LAN\Config;
 /**********************************************************************************************************************
 * autoload and include path
 */
-function autoload($class)
-{
-    $file = str_replace(array('_', '\\'), '/', $class).'.php';
-
-    if ($fullpath = stream_resolve_include_path($file)) {
-        include $fullpath;
-        return true;
-    }
-
-    return false;
-}
-
-spl_autoload_register("autoload");
-
-set_include_path(
-    implode(PATH_SEPARATOR, array(get_include_path())).PATH_SEPARATOR
-            .dirname(__FILE__) . '/lib/Epoch/src'.PATH_SEPARATOR //path to Epoch's src dir.
-            .dirname(__FILE__) . '/lib'.PATH_SEPARATOR
-            .dirname(__FILE__) . '/src'.PATH_SEPARATOR
-);
+require __DIR__ . '/vendor/autoload.php';
 
 /**********************************************************************************************************************
 * php related settings
---
+++ vendor/cboden/Ratchet/tests/Ratchet/Tests/Mock/Component.php
@@ -0,0 +1,36 @@
+<?php
+namespace Ratchet\Tests\Mock;
+use Ratchet\MessageComponentInterface;
+use Ratchet\WebSocket\WsServerInterface;
+use Ratchet\Tests\Mock\Socket as MockSocket;
+use Ratchet\ConnectionInterface;
+
+class Component implements MessageComponentInterface, WsServerInterface {
+    public $last = array();
+
+    public $protocols = array();
+
+    public function __construct(ComponentInterface $app = null) {
+        $this->last[__FUNCTION__] = func_get_args();
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->last[__FUNCTION__] = func_get_args();
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $this->last[__FUNCTION__] = func_get_args();
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->last[__FUNCTION__] = func_get_args();
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $this->last[__FUNCTION__] = func_get_args();
+    }
+
+    public function getSubProtocols() {
+        return $this->protocols;
+    }
+}
