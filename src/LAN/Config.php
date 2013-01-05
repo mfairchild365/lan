9ff50e3c63a4809faffa8355d80866d72d8e28ba
mfairchild365 <mfairchild365@gmail.com>
2013-01-04 20:22:14 -0600

    Update config for new steam vars

--- src/LAN/Config.php
+++ src/LAN/Config.php
@@ -13,12 +13,17 @@ class Config
         'SERVER_ETH'       => 'eth0', //SERVER ETHERNET PORT (used when auto detecting IP address) (IE: 'eth0' or 'en1')
         'APPLICATION_NAME' => 'lan',  //APPLICATION NAME (ie 192.168.1.5:8000/APPLICATION_NAME)
         'WWW_PATH'         => '/',    //Path to the www directory from the server's IP address. (IE: '/lan/www/') (TRAILING SLASH IS IMPORTANT)
+        'CACHE_DIR'        => false,  //The Cache Dir
 
         //DB RELATED SETTINGS
         'DB_HOST'          => false,  //DATABASE HOST
         'DB_USER'          => false,  //DATABASE USER
         'DB_PASSWORD'      => false,  //DATABASE PASSWORD
         'DB_NAME'          => false,  //DATABASE NAME
+
+        //STEAM SETTINGS
+        'STEAM_API_KEY'            => false,  //Your steam cache key
+        'STEAM_CACHE_TTL_PROFILES' => 15,     //Time to live in seconds for cache files
     );
 
     private function __construct()
@@ -39,6 +44,11 @@ class Config
             }
 
             return str_replace("\n","", exec("ifconfig " . self::$data['SERVER_ETH'] . " | grep 'inet addr' | awk -F':' {'print $2'} | awk -F' ' {'print $1'}"));
+        }
+
+        //Special default case: CACHE_DIR
+        if ($key == 'CACHE_DIR' && self::$data[$key] == false) {
+            return  dirname(dirname(dirname(__FILE__))) . "/tmp/";
         }
 
         return self::$data[$key];
